c5abcc48da2bf37525c0728e7e02dc6cd81a9a3a
ranixaurus <ranixaurus@gmail.com>
2022-07-08 17:07:38 +0700

    update 1
    Better improvement

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const { on } = require("events");
 const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const dayjs = require("dayjs");
 
-const port = 3000;
+const port = 5000;
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
 	
 	socket.on("messageFromClient", (message) => {
-		io.emit("messageFromServer", message);
+		const sendContent = {
+			"message": message,
+			"timestamp": dayjs().format("YYYY-MM-DD HH:mm:ss")
+		};
+		io.emit("messageFromServer", sendContent);
 	});
 
 });
 
 server.listen(process.env.PORT || port, () => {
 	console.log(`Hello World at port ${port}!`);
-})
+});
